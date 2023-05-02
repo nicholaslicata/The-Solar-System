6af06a1bc3ec0f3238492f7eaa8233830c8e7ffd
Nicholas Licata <nicholas.licata730@gmail.com>
2023-05-01 21:04:43 -0400

    initial upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+
+function Navbar() {
+    return (
+        <header>
+            <div>
+                <p>The Solar System</p>
+            </div>
+            <nav>
+        
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar;
