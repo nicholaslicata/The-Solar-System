e7d80f74dad3266d5e5fd9b14e7f08bc0d31f848
Nicholas Licata <nicholas.licata730@gmail.com>
2023-05-10 22:11:38 -0400

    added section buttons

--- src/pages/Mercury.js
+++ src/pages/Mercury.js
@@ -1,16 +1,35 @@
+import Buttons from '../components/Buttons';
 
-function Mercury() {
+function Mercury({overview, handleOverview, surface, handleSurface, atmosphere, handleAtmosphere, orbit, handleOrbit}) {
     return (
         <main className='planet-container'>   
+            <Buttons overview={overview} handleOverview={handleOverview} surface={surface} handleSurface={handleSurface} atmosphere={atmosphere} handleAtmosphere={handleAtmosphere} orbit={orbit} handleOrbit={handleOrbit}/>
             <div className='planet-text-container'>
               <img className='planet-img' src={require('../assets/mercury.png')} alt='Planet Mercury' />
               <div className='planet-text-content'>
                 <h1 className='planet-h1'>Mercury</h1>
+                {overview === true ?
                 <p className='planet-p'>
                     Mercury is the first planet from the Sun and the smallest planet in the solar system.
                     It is a terrestrial planet with a heavily cratered surface due to the planet having no geological
                     activity and an extremelty tenuous atmosphere.
+                </p>  : surface === true ?
+                <p className='planet-p'>
+                    Mercury's surface has a similar apperance to that of the Earth's moon with many craters caused
+                    by collisions with meteoroids and comets. The surface can reach 800 degrees Fahrenheit during the day 
+                    and drop to minus 290 degrees Fahrenheit at night.
+                </p>  : atmosphere === true ?
+                <p className='planet-p'>
+                    Mercury has a very tenous atmosphere that is instead commonly referred to as an exosphere, which is made up
+                    of atoms blasted off the surface by solar winds and striking meteoroids. This exosphere 
+                    is composed mostly of oxygen, sodium, hydrogen, helium, and postassium.
                 </p>
+                    : orbit === true ?
+                <p className='planet-p'>
+                    Mercury's orbit can take the planet as close as 29 million miles and as far as 43 million miles from the Sun. 
+                    Travelling at close to 29 miles per second, Mercury completes one rotation every 59 Earth 
+                    days. It's axis of rotation is just 2 degrees.
+                </p> : null}               
               </div>
             </div>
             <div className='planet-facts-container'>
