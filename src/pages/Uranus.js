06ce1251a4d12938a75858cc050eaf90dddb5f3d
Nicholas Licata <nicholas.licata730@gmail.com>
2023-05-13 15:19:00 -0400

    added section text

--- src/pages/Uranus.js
+++ src/pages/Uranus.js
@@ -1,16 +1,40 @@
+import { useEffect } from 'react';
+import Buttons from '../components/Buttons';
 
-function Uranus() {
+function Uranus({ overview, setOverview, handleOverview, surface, setSurface, handleSurface, atmosphere, setAtmosphere, handleAtmosphere, orbit, setOrbit, handleOrbit }) {
+    useEffect(() => {
+        setOverview(true);
+        setSurface(false);
+        setAtmosphere(false);
+        setOrbit(false);
+    }, [])
+
     return (
         <main className='planet-container'>   
+          <Buttons overview={overview} handleOverview={handleOverview} surface={surface} handleSurface={handleSurface} atmosphere={atmosphere} handleAtmosphere={handleAtmosphere} orbit={orbit} handleOrbit={handleOrbit}/>
             <div className='planet-text-container'>
               <img className='planet-img' src={require('../assets/uranus.png')} alt='Planet Uranus' />
               <div className='planet-text-content'>
                 <h1 className='planet-h1'>Uranus</h1>
+                {overview === true ? 
                 <p className='planet-p'>
                     Uranus is the seventh planet from the Sun and is a gaseous ice giant. Most of the planet is made out of
                     water, ammomia, and methane in a supercritical phase of matter. The planet has the lowest minimum temperature out 
                     of all the Solar System's planets.
-                </p>
+                </p> : surface === true ?
+                <p className='planet-p'>
+                    Uranus doesn't have a true surface, as the planet is mostly swirling fluids. Most of the planet's mass is made up 
+                    of water, ammonia, and methane above a small rocky core. Near the core temperatures can reach up to 9,000 degrees Fahrenheit.
+                </p> : atmosphere === true ? 
+                <p className='planet-p'>
+                    Uranus' atmosphere is mostly hydrogen and helium with trace amounts of methane, water, and ammonia. The methane is what 
+                    gives Uranus it's blue coloring. Atmospheric wind speeds can reach upwards of 560 miles per hour.
+                </p> : orbit === true ? 
+                <p className='planet-p'>
+                    Uranus completes one full rotation in about 17 hours and makes a complete orbit around the Sun in about 30,687 Earth days. 
+                    It's axis of rotation is 97.77 degrees, which causes the most extreme seasons in the Solar System. Like Venus, it rotates backwards
+                    on it's axis.
+                </p> : null}
               </div>
             </div>
             <div className='planet-facts-container'>
